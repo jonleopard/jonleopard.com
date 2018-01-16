e9cd98342f9a737b56f3f9298b4ef9b5b5788c2b
Jon Leopard <jonlprd@gmail.com>
2018-01-16 13:12:29 +0100

    Added styled components, site metadata, and google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    author: 'Jon Leopard',
+    title: `Jon Leopard | Web Developer`,
+    siteUrl: 'https://jonleopard.com',
+    defaultTitle: 'Jon Leopard | Web Developer',
+    defaultDescription:
+      'Jon Leopard is a Javascript Web Developer with skills in both Front-end and Back-end technologies.',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-88655191-1',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+  ],
 };
