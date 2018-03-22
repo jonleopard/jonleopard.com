4377b122108202a2de34d751fc5740fba94650f9
Jon Leopard <jontron2000@gmail.com>
2018-03-22 16:54:41 +0100

    Fixed path

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images/`
+        path: `${__dirname}/src/static-assets/images/`
       }
     },
     {
