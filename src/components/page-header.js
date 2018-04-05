5fbb794cbb60511c26be8629b0ba7df1368e30d3
Jon Leopard <jontron2000@gmail.com>
2018-04-05 11:27:06 +0200

    Fixed margin and padding display issue on mobile

--- src/components/page-header.js
+++ src/components/page-header.js
@@ -4,7 +4,7 @@ import { Box } from 'grid-styled';
 import { H1, Text } from 'components/typography';
 
 const PageHeader = ({ title, subTitle }) => (
-  <Box pt={[24, 48]} pb={[8, 16]}>
+  <Box pt={[12, 24]} pb={[4, 8]}>
     <H1>{title}</H1>
     {subTitle ? <Text fontSize={[0, 1, 2]}>{subTitle}</Text> : null}
   </Box>
