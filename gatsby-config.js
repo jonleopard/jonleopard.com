c33bc5d1c12bc8ed0af0afb5d483fe09f32f7bc4
Jon Leopard <jontron2000@gmail.com>
2018-06-26 10:27:55 +0200

    Removed data folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,6 @@ module.exports = {
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
-				name: `data`,
-				path: `${__dirname}/src/data/`,
-			},
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
 				name: `images`,
 				path: `${__dirname}/src/static-assets/images/`,
 			},
