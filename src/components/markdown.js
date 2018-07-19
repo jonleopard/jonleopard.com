241f6abb6cd7a885e3aa4f1cca529a703e42b290
Jon Leopard <jontron2000@gmail.com>
2018-07-19 14:39:17 +0200

    Markdown blockquotes added

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -1,5 +1,4 @@
 import { Box } from 'grid-styled';
-import styled from 'styled-components';
 import { H2 } from 'components/typography';
 
 const Markdown = Box.extend`
@@ -89,6 +88,39 @@ const Markdown = Box.extend`
     padding-bottom: 1px;
     border-bottom: 1px solid ${props => props.theme.colors.primary};
   }
+
+  blockquote {
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    margin: 1.75em 0 1.75em -2.2em;
+    padding: 0 0 0 1.75em;
+    border-left: #4a4a4a 0.4em solid;
+  }
+
+  blockquote p {
+    margin: 0.8em 0;
+    font-style: italic;
+  }
+
+  blockquote small {
+    display: inline-block;
+    margin: 0.8em 0 0.8em 1.5em;
+    font-size: 0.9em;
+    color: #ccc;
+  }
+
+  blockquote small::before {
+    content: '\\2014 \\00A0';
+  }
+
+  blockquote cite {
+    font-weight: 700;
+  }
+
+  blockquote cite a {
+    font-weight: normal;
+  }
+  k
 
   /*
 	* Dracula Theme for Prism.JS
