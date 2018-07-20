8db464598d8e5e50835f6d3a8a77fc6ee48b9b36
Jon Leopard <jontron2000@gmail.com>
2018-07-20 23:33:49 +0200

    ugh

--- src/html.js
+++ src/html.js
@@ -39,7 +39,8 @@ module.exports = class HTML extends React.Component {
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
+          /> 
+          <meta name="twitter:card" content="summary_large_image" />
           <meta
             name="twitter:image"
             content={`https://jonleopard.com${twitterCard}`}
