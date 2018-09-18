3a539a1429d9bfdb65272cf3de08d2ae97b86085
Jon Leopard <jontron2000@gmail.com>
2018-09-18 16:22:07 +0200

    Add temporary theme file

---
+++ src/components/theme.js
@@ -0,0 +1,13 @@
+export default {
+  fontWeight: [300, 400, 500, 600],
+  fontSizes: [12, 14, 16, 18, 20, 24, 32, 48, 64, 72],
+  colors: {
+    background: "#F5F5F5",
+    heading: "#000000",
+    text: "#000000",
+    textHover: "#F59AF0",
+    border: "#e6e9ef",
+    link: "#000000",
+    primary: "#F59AF0"
+  }
+};
