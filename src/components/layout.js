cc7c9af2129c5a7d978e3dd4e87895cf0feefd8a
Jon Leopard <jontron2000@gmail.com>
2018-09-18 15:12:09 +0200

    Prettier

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import Footer from 'components/footer';
-import Header from 'components/header';
-import GlobalStyles from 'components/global-styles';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Footer from "components/footer";
+import Header from "components/header";
+import GlobalStyles from "components/global-styles";
 
 const theme = {
   fontWeight: [300, 400, 500, 600],
   fontSizes: [12, 14, 16, 18, 20, 24, 32, 48, 64, 72],
   colors: {
-    background: '#F5F5F5',
-    heading: '#000000',
-    text: '#000000',
-    textHover: '#F59AF0',
-    border: '#e6e9ef',
-    link: '#000000',
-    primary: '#F59AF0',
-  },
+    background: "#F5F5F5",
+    heading: "#000000",
+    text: "#000000",
+    textHover: "#F59AF0",
+    border: "#e6e9ef",
+    link: "#000000",
+    primary: "#F59AF0"
+  }
 };
 
 const Inner = styled.div`
@@ -37,4 +37,4 @@ const Layout = ({ children }) => (
   </ThemeProvider>
 );
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import { graphql } from 'gatsby';
-import Helmet from 'react-helmet';
-import { Box } from 'grid-styled';
-import styled from 'styled-components';
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Helmet from "react-helmet";
+import { Box } from "grid-styled";
+import styled from "styled-components";
 
-import Layout from 'components/layout';
-import { Text, P } from 'components/typography';
-import Section, { SectionTitle } from 'components/section';
+import Layout from "components/layout";
+import { Text, P } from "components/typography";
+import Section, { SectionTitle } from "components/section";
 
 const PostDate = styled(Text)`
   font-size: 0.8em;
@@ -19,25 +18,23 @@ const Index = ({ data }) => {
   const meta = data.site.siteMetadata;
 
   return (
-    <Layout>
-      <main>
+    <main>
+      <Layout>
         <Helmet title={meta.defaultTitle}>
           <meta name="twitter:title" content={meta.defaultTitle} />
           <meta name="twitter:description" content={meta.defaultDescription} />
         </Helmet>
         <Section pt={[20, 40]}>
           <h1>
-            Hello, my name is Jon. <br />
-{' '}
-I am a web developer based in Paris.
-</h1>
+            Hello, my name is Jon. <br /> I am a web developer based in Paris.
+          </h1>
         </Section>
 
         <SectionTitle>Recent articles</SectionTitle>
         {posts.map(({ node: post }) => (
           <Box mb={2} key={post.title}>
             <Text>
-              <Link to={post.slug} style={{ color: 'black' }}>
+              <Link to={post.slug} style={{ color: "black" }}>
                 {post.title}
                 <PostDate is="time" dateTime={post.date}>
                   {post.date}
@@ -46,8 +43,8 @@ I am a web developer based in Paris.
             </Text>
           </Box>
         ))}
-      </main>
-    </Layout>
+      </Layout>
+    </main>
   );
 };
 
