20aa9bfee189655c7292cf4511e0f194d9cd517b
Jon Leopard <jontron2000@gmail.com>
2018-09-25 19:47:37 +0200

    Commit work

---
+++ src/components/GatsbyLink.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link as GatsbyLink } from 'gatsby';
+import { Text } from 'jonleopard-design-system';
+
+
+
+const Link = ({ children, to, ...other }) => {
+  // Tailor the following test to your environment.
+  // This example assumes that any internal link (intended for Gatsby)
+  // will start with exactly one slash, and that anything else is external.
+  const internal = /^\/(?!\/)/.test(to);
+
+  // Use Gatsby Link for internal links, and <a> for others
+  if (internal) {
+    return (
+      <GatsbyLink to={to} {...other}>
+        {children}
+      </GatsbyLink>
+    );
+  }
+  return (
+    <a href={to} {...other}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
