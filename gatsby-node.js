e736b03eb63d4729d16b463aacc7cd469e7b3f4e
Jon Leopard <jontron2000@gmail.com>
2019-04-23 14:43:27 +0200

    Fixed urls

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,27 +14,27 @@ exports.createPages = ({ graphql, actions }) => {
     },
     { 
       f: `/update-on-learning-javascript`,
-      t: `blog/update-on-learning-javascript`
+      t: `/blog/update-on-learning-javascript`
     },
     { 
       f: `/learning-how-to-program`,
-      t: `blog/learning-how-to-program`
+      t: `/blog/learning-how-to-program`
     },
     { 
       f: `/new-machine-setup-automation`,
-      t: `blog/new-machine-setup-automation`
+      t: `/blog/new-machine-setup-automation`
     },
     { 
       f: `/upgrading-ghost-inside-docker`,
-      t: `blog/upgrading-ghost-inside-docker`
+      t: `/blog/upgrading-ghost-inside-docker`
     },
     { 
       f: `/installing-ghost-and-nginx-on-ubuntu-16-04`,
-      t: `blog/installing-ghost-and-nginx-on-ubuntu-16-04`
+      t: `/blog/installing-ghost-and-nginx-on-ubuntu-16-04`
     },
     { 
       f: `/dotfile-management-with-gnu-stow`,
-      t: `blog/dotfile-management-with-gnu-stow`
+      t: `/blog/dotfile-management-with-gnu-stow`
     },
   ];
 
@@ -43,6 +43,7 @@ exports.createPages = ({ graphql, actions }) => {
       fromPath: f,
       redirectInBrowser: true,
       toPath: t,
+      isPermanent: true,
     })
     // Uncomment next line to see forEach in action during build
     console.log('\nRedirecting:\n' + f + '\nTo:\n' + t + '\n');
