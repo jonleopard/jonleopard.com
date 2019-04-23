fd350fbc2d2ba42d82c95d43c4e7e3c5d1be08fd
Jon Leopard <jontron2000@gmail.com>
2019-04-23 14:17:40 +0200

    Add redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,50 @@ const path = require('path')
 
 // get rid of slash
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage, createRedirect } = actions;
+
+  // Redirects
+  let redirectBatch = [
+    { 
+      f: `/react-stateless-functional-components`,
+      t: `/blog/react-stateless-functional-components`
+    },
+    { 
+      f: `/update-on-learning-javascript`,
+      t: `blog/update-on-learning-javascript`
+    },
+    { 
+      f: `/learning-how-to-program`,
+      t: `blog/learning-how-to-program`
+    },
+    { 
+      f: `/new-machine-setup-automation`,
+      t: `blog/new-machine-setup-automation`
+    },
+    { 
+      f: `/upgrading-ghost-inside-docker`,
+      t: `blog/upgrading-ghost-inside-docker`
+    },
+    { 
+      f: `/installing-ghost-and-nginx-on-ubuntu-16-04`,
+      t: `blog/installing-ghost-and-nginx-on-ubuntu-16-04`
+    },
+    { 
+      f: `/dotfile-management-with-gnu-stow`,
+      t: `blog/dotfile-management-with-gnu-stow`
+    },
+  ];
+
+  redirectBatch.forEach(({ f, t }) => {
+    createRedirect({
+      fromPath: f,
+      redirectInBrowser: true,
+      toPath: t,
+    })
+    // Uncomment next line to see forEach in action during build
+    console.log('\nRedirecting:\n' + f + '\nTo:\n' + t + '\n');
+})
+
   return graphql(
     `
       {
