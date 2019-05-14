3e45ba818fdd0a2707cc5f7d97dbd5783882ba55
Jon Leopard <jontron2000@gmail.com>
2019-05-14 13:02:57 +0200

    Add service worker dialog

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,14 @@
 // I put these in the layout.js component instead
 // require('typeface-rubik')
 // require('typeface-lato')
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
