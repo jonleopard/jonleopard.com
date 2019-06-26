1bd4624c880bbb6bf60ff1df8434acfbd8f07dda
Jon Leopard <jontron2000@gmail.com>
2019-06-26 10:03:44 +0200

    Changed components to use .jsx extension

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable */
 // TODO: disabling until I can reconfigure rules
-import React, { useState, useLayoutEffect, useContext } from 'react'
-import styled, { createGlobalStyle, ThemeProvider, css } from 'styled-components'
-import { Flex, Box } from 'rebass'
-import { normalize } from 'styled-normalize'
-import { merge, get } from 'lodash'
-import { useTransition, animated } from 'react-spring'
+import React, { useState, useLayoutEffect, useContext } from "react";
+import styled, {
+  createGlobalStyle,
+  ThemeProvider,
+  css
+} from "styled-components";
+import { Flex, Box } from "rebass";
+import { normalize } from "styled-normalize";
+import { merge, get } from "lodash";
+import { useTransition, animated } from "react-spring";
 
 // Fonts
-import Rubik from 'typeface-rubik'
-import Lato from 'typeface-lato'
+import Rubik from "typeface-rubik";
+import Lato from "typeface-lato";
 
 // Components
-import theme from './theme'
-import SEO from './SEO'
-import Footer from './footer'
-import NavBar from './navbar'
+import theme from "./theme";
+import SEO from "./SEO";
+import Footer from "./footer";
+import NavBar from "./navbar";
 
 const GlobalStyle = createGlobalStyle`  
   body {
@@ -35,66 +39,66 @@ const GlobalStyle = createGlobalStyle`
     color: ${props => props.theme.colors.primary}; 
 
   ${normalize}
-}`
+}`;
 
 // Create some context for other components
-export const Context = React.createContext()
-export const useAppContext = () => useContext(Context)
+export const Context = React.createContext();
+export const useAppContext = () => useContext(Context);
 
 // Color modes
-const modes = ['light', 'dark']
+const modes = ["light", "dark"];
 
 const getTheme = mode =>
   merge({}, theme, {
-    colors: get(theme.colors.modes, mode, theme.colors),
-  })
+    colors: get(theme.colors.modes, mode, theme.colors)
+  });
 
 const Wrapper = styled(Box)`
   max-width: 560px;
   margin: 0 auto;
   padding-left: 19px;
   padding-right: 19px;
-`
+`;
 
 const Layout = ({ children, location }) => {
   // Color mode hook
-  const [mode, setMode] = useState(modes[0])
+  const [mode, setMode] = useState(modes[0]);
 
-  const theme = getTheme(mode)
+  const theme = getTheme(mode);
   if (location) {
-    theme.colors = merge({}, theme.colors, colors)
+    theme.colors = merge({}, theme.colors, colors);
   }
 
   useLayoutEffect(() => {
-    const initialMode = window.localStorage.getItem('mode') || modes[0]
+    const initialMode = window.localStorage.getItem("mode") || modes[0];
     if (initialMode && initialMode !== mode) {
-      setMode(initialMode)
+      setMode(initialMode);
     }
-  }, [])
+  }, []);
 
   useLayoutEffect(() => {
-    window.localStorage.setItem('mode', mode)
-  }, [mode])
+    window.localStorage.setItem("mode", mode);
+  }, [mode]);
 
   const cycleMode = () => {
-    const i = (modes.indexOf(mode) + 1) % modes.length
-    setMode(modes[i])
-  }
+    const i = (modes.indexOf(mode) + 1) % modes.length;
+    setMode(modes[i]);
+  };
 
   const context = {
     modes,
     mode,
     setMode,
-    cycleMode,
-  }
+    cycleMode
+  };
 
   // Page transition hook
-  const [toggle, set] = useState(false)
+  const [toggle, set] = useState(false);
   const transitions = useTransition(toggle, null, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
-    leave: { opacity: 0 },
-  })
+    leave: { opacity: 0 }
+  });
 
   return (
     <>
@@ -127,7 +131,7 @@ const Layout = ({ children, location }) => {
         </ThemeProvider>
       </Context.Provider>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
