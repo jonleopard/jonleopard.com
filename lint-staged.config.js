62f522ecbaa23f613427d5dfca52d9a40dce6870
Jon Leopard <jontron2000@gmail.com>
2019-10-03 13:57:59 +0200

    Working on CI

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '*.{js,jsx}': ['eslint --ignore-path .gitignore --fix', 'git add'],
+  '*.+(json|yml|yaml|css|less|scss|md|graphql|mdx)': [
+    'prettier --write',
+    'git add',
+  ],
+}
