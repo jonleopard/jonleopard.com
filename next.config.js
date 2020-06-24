0ac2ea5cea30690d4103a5d95e74af1048622d63
Jon Leopard <jontron2000@gmail.com>
2020-06-24 21:03:22 +0200

    See if this removes error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withSourceMaps = require('@zeit/next-source-maps');
 
 module.exports = withCSS(
   withSourceMaps({
-    target: 'serverless',
     webpack: (config, { isServer }) => {
       // Fixes npm packages that depend on `fs` module
       if (!isServer) {
