c4879cf4d238a71e9808eb3558cb5d3af0a95d87
Jon Leopard <jontron2000@gmail.com>
2020-06-25 16:08:28 +0200

    Footer fix + fontawesome icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,12 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithub,
+  faTwitter,
+  faStackOverflow,
+  faTelegram,
+} from '@fortawesome/free-brands-svg-icons';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
 import 'twin.macro';
 
@@ -9,35 +17,31 @@ const Line = styled.hr`
 function Footer() {
   const WHAT_YEAR_IS_IT = new Date().getFullYear();
   return (
-    <div>
-      <div tw="max-w-2xl pt-10 pb-6 px-8 mx-auto">
-        <footer tw="flex text-xs font-bold uppercase justify-between">
-          <a href="https://github.com/jonleopard" tw="pr-5">
-            github
+    <footer>
+      <div tw="w-full flex-col max-w-2xl pt-10 pb-6 px-8 mx-auto flex text-xs font-bold uppercase text-gray-800 items-center">
+        <div tw="flex justify-between w-1/2">
+          <a href="https://github.com/jonleopard">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
           </a>
-          <a href="https://twitter.com/jonlprd" tw="pr-5">
-            twitter
+          <a href="https://twitter.com/jonlprd">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
           </a>
-          <a
-            href="https://stackoverflow.com/users/10541736/jon-leopard"
-            tw="pr-5"
-          >
-            stack overflow
+          <a href="https://stackoverflow.com/users/10541736/jon-leopard">
+            <FontAwesomeIcon icon={faStackOverflow} size="2x" />
           </a>
-          <a href="https://t.me/hiJon" tw="pr-5">
-            telegram
+          <a href="https://t.me/hiJon">
+            <FontAwesomeIcon icon={faTelegram} size="2x" />
           </a>
-          <a
-            href="https://keybase.io/jonleopard/pgp_keys.asc"
-            tw="pr-5"
-          >
-            gpg
+          <a href="https://keybase.io/jonleopard/pgp_keys.asc">
+            <FontAwesomeIcon icon={faKey} size="2x" />
           </a>
-          <div>
+        </div>
+        <div tw="mt-2">
+          <span>
             &copy;
-            <span> {WHAT_YEAR_IS_IT}</span>
-          </div>
-        </footer>
+            {WHAT_YEAR_IS_IT}
+          </span>
+        </div>
       </div>
       <div tw="grid grid-cols-1 gap-1">
         <Line height="6px" color="#62BB47" />
@@ -47,7 +51,7 @@ function Footer() {
         <Line height="2px" color="#963D97" />
         <Line height="1px" color="#009ddc" />
       </div>
-    </div>
+    </footer>
   );
 }
 
--- components/Providers/index.jsx
+++ components/Providers/index.jsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 // import GlobalStyles from '../GlobalStyles';
 import { ThemeProvider } from 'styled-components';
-import SEO from './SEO';
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'tailwindcss/dist/base.min.css';
+
+import SEO from './SEO';
+
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 const theme = {
   colors: {
