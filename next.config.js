631dd696a88fa693c653be7f92c792cbe73033e3
Jon Leopard <jontron2000@gmail.com>
2021-01-21 22:23:04 +0100

    Fixed error (deprecated feature)

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
-const withCSS = require('@zeit/next-css');
-const withSourceMaps = require('@zeit/next-source-maps');
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes packages that depend on fs/module module
+    if (!isServer) {
+      config.node = { fs: 'empty', module: 'empty' };
+    }
 
-module.exports = withCSS(
-  withSourceMaps({
-    target: 'serverless',
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: 'empty',
-        };
-      }
-      return config;
-    },
-  }),
-);
+    return config;
+  },
+};
