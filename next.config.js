57baebf67033eca4bbf1d01cb6d0c72800e28cea
Jon Leopard <contact@jonleopard.com>
2021-03-31 10:39:04 +0200

    Package upgrades @ NextJS v10.1.2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  target: 'experimental-serverless-trace',
-  webpack: (config, { isServer }) => {
-    // Fixes packages that depend on fs/module module
-    if (!isServer) {
-      config.node = { fs: 'empty', module: 'empty' };
-    }
-
-    return config;
+  future: {
+    webpack5: true,
   },
 };
