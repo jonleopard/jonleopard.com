52efc765f1d4317358539b01fd05859d3d1519aa
Jon Leopard <jontron2000@gmail.com>
2021-07-09 18:55:29 +0200

    Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,51 @@
 import * as React from 'react';
 import 'twin.macro';
 import Emoji from 'a11y-react-emoji';
+import Link from 'next/link';
+import { PopupButton } from 'react-calendly';
 import { getLayout } from '../components/SiteLayout';
 
 function Index() {
   return (
     <>
-      <div tw="flex py-10 max-w-3xl mx-auto items-center flex-col">
-        <h1 tw="block text-lg md:text-2xl lg:text-4xl font-bold leading-none mb-2">
-          Hey! I'm Jon <Emoji symbol="👋🏻" label="Waving" />
-        </h1>
-        <div tw="leading-tight text-gray-700">
-          <p tw="mb-2">
-            <Emoji tw="mr-1" symbol="👨🏻‍💻" label="Man Technologist" /> Web
-            developer & Indie Hacker
-          </p>
-          <p tw="mb-2">
-            <Emoji tw="mr-2" symbol="🌎" label="Earth" />
-            Remote worker
-          </p>
-          <p tw="mb-2">
-            <Emoji tw="mr-2" symbol="👾" label="Space Invader" />
-            Gaming, photography, outdoors <br />
-          </p>
-          <p tw="mb-2">
-            <Emoji tw="mr-2" symbol="🇺🇸" label="American Flag" />→{' '}
-            <Emoji tw="mr-2" symbol="🇫🇷" label="France Flag" />
-            U.S Expat living in Paris <br />
-          </p>
-        </div>
+      <div tw="flex py-10 max-w-3xl mx-auto flex-col">
+        <main tw="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+          <div tw="sm:text-center lg:text-left">
+            <h1 tw="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <span tw="block xl:inline">
+                Hey! I'm Jon <Emoji symbol="👋🏻" label="Waving" />
+              </span>{' '}
+              <span tw="block text-5xl text-gray-600 xl:inline">
+                {' '}
+                I'm a Front End Developer and API Specialist living in Paris, FR
+              </span>
+            </h1>
+            <p tw="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              I help individuals & enterprises solve their online problems and
+              I’m currently{' '}
+              <span tw="bg-green-100 text-green-800 inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0">
+                available
+              </span>{' '}
+              for work!
+            </p>
+            <div tw="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div className="rounded-md shadow">
+                <PopupButton
+                  tw="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                  url="https://calendly.com/contact-4394/15min"
+                  text="Schedule a call"
+                />
+              </div>
+              <div tw="mt-3 sm:mt-0 sm:ml-3">
+                <Link href="/about">
+                  <a tw="w-full cursor-pointer flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                    Learn more about me
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     </>
   );
