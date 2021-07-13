0bd9c2cb24fe30a208e1d26cb6cb0b86c68fb8b8
Jon Leopard <jontron2000@gmail.com>
2021-07-13 09:28:09 +0200

    Changed className to tw prefix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ function Index() {
               for work!
             </p>
             <div tw="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-              <div className="rounded-md shadow">
+              <div tw="rounded-md shadow">
                 <PopupButton
                   tw="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   url="https://calendly.com/contact-4394/15min"
