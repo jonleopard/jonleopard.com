750567568693386036f22f6cb9b06815500b23c2
Jon Leopard <contact@jonleopard.com>
2021-09-10 11:34:17 +0200

    Package upgrades/formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import "twin.macro";
-import Emoji from "a11y-react-emoji";
-import Link from "next/link";
-import { PopupButton } from "react-calendly";
-import { getLayout } from "../components/SiteLayout";
+import * as React from 'react'
+import 'twin.macro'
+import Emoji from 'a11y-react-emoji'
+import Link from 'next/link'
+import { PopupButton } from 'react-calendly'
+import { getLayout } from '../components/SiteLayout'
 
 function Index() {
   return (
@@ -14,18 +14,18 @@ function Index() {
             <h1 tw="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span tw="block xl:inline">
                 Hey! I'm Jon <Emoji symbol="👋🏻" label="Waving" />
-              </span>{" "}
+              </span>{' '}
               <span tw="block text-5xl text-gray-600">
-                {" "}
+                {' '}
                 I'm a Front End Developer and API Specialist living in Paris, FR
               </span>
             </h1>
             <p tw="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
               I help individuals & enterprises solve technical problems and I’m
-              currently{" "}
+              currently{' '}
               <span tw="bg-green-100 text-green-800 inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0">
                 available
-              </span>{" "}
+              </span>{' '}
               for work!
             </p>
             <div tw="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
@@ -48,9 +48,9 @@ function Index() {
         </main>
       </div>
     </>
-  );
+  )
 }
 
-Index.getLayout = getLayout;
+Index.getLayout = getLayout
 
-export default Index;
+export default Index
