0309fc456a4e0dad62e13a42c85ae7d6d62fc7cc
Jon Leopard <contact@jonleopard.com>
2021-10-06 16:10:33 +0200

    Add JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-};
+}
