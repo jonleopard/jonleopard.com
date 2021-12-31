93e84da665a56cd82a3ad61c65edd3fad087d0d0
Jon Leopard <contact@jonleopard.com>
2021-12-31 11:19:47 -0700

    Put redirects in next config file

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@
 const nextConfig = {
   swcMinify: false,
   future: { webpack5: true },
+  async redirects() {
+    return [
+      {
+        source: '/dotfile-management-with-gnu-stow',
+        destination: '/blog/dotfile-management-with-gnu-stow',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
